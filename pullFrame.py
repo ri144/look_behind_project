7e045896cf1e1bfc32cf7d0e97a1c26169d36e35
ri144 <ri144@github.com>
2017-10-04 14:35:46 -0400

    faster frame pulling

--- pullFrame.py
+++ pullFrame.py
@@ -6,10 +6,14 @@ from calc_hist import *
 
 filename = sys.argv[1]
 frame = sys.argv[2]
-print frame
+#print frame
 cap = cv2.VideoCapture(filename)
-for x in range(int(frame)):
-	cap.read()	#read all the undesirable previous frames
+vid_length = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+#print cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, float(frame))
+#print cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+#for x in range(int(frame)):
+	#cap.read()	#read all the undesirable previous frames
 ret, frame = cap.read() #get the desired frame
 cv2.imshow('frame',frame)
 k = cv2.waitKey(10000) & 0xff
